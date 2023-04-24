88b4fd1751589e9cbb696828b98616026fb4f571
pintuprajapati <pintu1481999@gmail.com>
2023-04-24 14:47:32 +0530

    CRUD APIs using FBV

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import student_detail, student_list, student_create
+from .views import student_api
 
 urlpatterns = [
-    path('stuinfo/<int:pk>/',student_detail),
-    path('stuinfo/',student_list),
-    path('stucreate/', student_create)
+    path('studentapi/', student_api),
 ]
