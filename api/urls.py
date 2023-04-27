3de6c982f3baafa8358461d64f67b75a285e7f32
pintuprajapati <pintu1481999@gmail.com>
2023-04-27 12:21:32 +0530

    update: removed code from urls and views files for new methods

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import student_detail, student_list, student_create
-
-urlpatterns = [
-    path('stuinfo/<int:pk>/',student_detail),
-    path('stuinfo/',student_list),
-    path('stucreate/', student_create)
-]
