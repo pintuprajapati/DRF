0b93aae52e75d4e4c59fc7e85e13ab15e7f1cc64
pintuprajapati <pintu1481999@gmail.com>
2023-05-01 15:16:32 +0530

    feat: Added BasicAuthentication and IsAuthenticated permission

--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     """
@@ -17,6 +20,11 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     """
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    # Authentication and Permission
+    # IsAuthenticated - Whether user is admin or not, doesn't matter. If user is registered then user can access the API
+    authentication_classes = [BasicAuthentication] # adding BasicAuthentication to this API
+    permission_classes = [IsAuthenticated] # only authenticated users are allowed to access this API (User ID and Password needed)
 
 class StudentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
     """
