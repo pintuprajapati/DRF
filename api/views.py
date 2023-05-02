15e28f480467ada940f49932fa8d7c3d848c3a4f
pintuprajapati <pintu1481999@gmail.com>
2023-05-02 12:41:52 +0530

    update: added Authentication and Permission in FBV_api_view

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 # def student_api(request, pk=None): # For browsable API testing, you can uncomment this and test it
 def student_api(request):
     if request.method == 'GET':
