9977ed2f2359de74113a34c9753c3a971a313489
pintuprajapati <pintu1481999@gmail.com>
2023-05-02 16:45:49 +0530

    update: Generate Tokenusing CMD with more user details

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from .auth import CustomAuthToken
 
 """
 - First Create a Route() object and then register the class with Route()
@@ -21,16 +22,25 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls')), # To enable Login, Logout option in Browsable API
     path('gettoken/', obtain_auth_token),
+    path('custom-auth-token/', CustomAuthToken.as_view()), # to get more information of user
 ]
 """
+below command runs because of 'pip install httpie'
+
 We can get/create the token by hitting this url from cmd:
 http POST http://127.0.0.1:8000/api/gettoken/ username="admin" password="admin"
-
-above command runs because of 'pip install httpie'
-
 Output will be:
 {
     "token": "a33c619b47add6c1ab66b50569031d1fffb89a77"
+}
+
+-----------------------------
+http POST http://127.0.0.1:8000/api/custom-auth-token/ username="admin" password="admin"
+Output will be:
+{
+    "email": "admin@admin.com",
+    "token": "a33c619b47add6c1ab66b50569031d1fffb89a77",
+    "user_id": 1
 }
 """
 
