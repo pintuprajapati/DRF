fcdc39926251f8091aaaca6416fb770cedc177a5
pintuprajapati <pintu1481999@gmail.com>
2023-05-02 17:03:15 +0530

    update: Generate AuthToken using Signals

--- api/models.py
+++ api/models.py
@@ -5,4 +5,18 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     roll = models.IntegerField()
     city = models.CharField(max_length=100)
-    
+
+######################## Generate Token using Signal ########################
+"""
+As soon as user is created Token is generated
+For testing purpose you can create user from admin panela and check Generated Token in Token Table
+"""
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
